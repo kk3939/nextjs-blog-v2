884fd82c57512a416a0fa4aa395b23b85da51b9a
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-01-23 14:13:52 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
